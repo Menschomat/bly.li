e1e83d1bf26df8cda6a4c0efa846c4a9127175d1
Falco Weihrauch <falco@weihrauch.me>
2024-08-24 18:41:00 +0200

    moved to singel-file-components

--- src/services/front/src/app/components/generic/button-primary/button-primary.component.ts
+++ src/services/front/src/app/components/generic/button-primary/button-primary.component.ts
@@ -5,8 +5,21 @@ import { OAuthService } from 'angular-oauth2-oidc';
   selector: 'app-button-primary',
   standalone: true,
   imports: [],
-  templateUrl: './button-primary.component.html',
-  styleUrl: './button-primary.component.scss',
+  template: `
+    <button
+      (click)="triggerBtn($event)"
+      class="bg-animate p-0.5 rounded-lg from-indigo-400 via-pink-500 to-purple-500 bg-gradient-to-r"
+    >
+      <div
+        class="px-4 py-2 bg-white dark:bg-gray-900 rounded-md text-transparent hover:bg-transparent hover:text-white"
+      >
+        <span
+          class="bg-animate block bg-clip-text font-montserrat font-black leading-snug bg-clip-text bg-gradient-to-r from-indigo-400 via-pink-500 to-purple-500"
+          >shrink</span
+        >
+      </div>
+    </button>
+  `,
 })
 export class ButtonPrimaryComponent {
   @Output() click: EventEmitter<any> = new EventEmitter();
