d1401c5cfaad726713eb0fa217305b4827b9d932
Falco Weihrauch <falco@weihrauch.me>
2025-01-29 19:02:04 +0000

    update mongo-client to propper singleton

--- src/shared/mongo/mongo.go
+++ src/shared/mongo/mongo.go
@@ -3,6 +3,7 @@ package mongo
 import (
 	"context"
 	"log"
+	"sync"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -11,35 +12,44 @@ import (
 
 var client *mongo.Client
 
+var (
+	clientInstance *mongo.Client
+	clientOnce     sync.Once
+)
+
 const DATABASE string = "short_url_db"
 
 func GetMongoClient() (*mongo.Client, error) {
-	if client != nil {
-		return client, nil
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	var err error
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongodb:27017"))
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	// check the connection
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
+	clientOnce.Do(func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		var err error
+		client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongodb:27017"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		// check the connection
+		err = client.Ping(ctx, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
 	return client, nil
 }
+
 func CloseClientDB() {
-	if client == nil {
-		return
+	if clientInstance != nil {
+		log.Println("Closing MongoDB connection...")
+
+		// Create a timeout of 5 seconds for disconnection
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		// Disconnect the client
+		if err := clientInstance.Disconnect(ctx); err != nil {
+			log.Fatalf("Error disconnecting MongoDB: %v", err)
+		}
+
+		log.Println("MongoDB connection closed.")
 	}
-	err := client.Disconnect(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Connection to MongoDB closed.")
 }
