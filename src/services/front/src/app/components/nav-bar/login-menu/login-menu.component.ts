5b294b7c4375f9653121e5a9a0410b6ea6f9c1b3
Falco Weihrauch <falco@weihrauch.me>
2025-02-08 22:57:22 +0000

    add link to empty dash

--- src/services/front/src/app/components/nav-bar/login-menu/login-menu.component.ts
+++ src/services/front/src/app/components/nav-bar/login-menu/login-menu.component.ts
@@ -2,35 +2,39 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { filter, map, Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-    selector: 'app-login-menu',
-    imports: [CommonModule],
-    template: `
+  selector: 'app-login-menu',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  template: `
     <div
       *ngIf="!(curUsrName | async)"
-      class="transition-colors duration-300 transform text-gray-800 dark:text-gray-200"
+      class="flex flex-col sm:flex-row  items-end gap-4 transition-colors duration-300 transform text-gray-800 dark:text-gray-200"
     >
       <a (click)="login()" class="text-lg cursor-pointer align-middle">
-        Login<i class="fa-solid fa-arrow-right-to-bracket"></i
+        Login<i class="ml-2 fa-solid fa-arrow-right-to-bracket"></i
       ></a>
     </div>
     <div
       *ngIf="curUsrName | async as username"
       class=" flex flex-col sm:flex-row  items-end gap-4 transition-colors duration-300 transform text-gray-800 dark:text-gray-200"
     >
-      <a class="text-lg cursor-pointer align-middle "
-        >{{ username }}<i class="ml-2 fa-regular fa-user"></i
-      ></a>
       <a
-        (click)="logout()"
-     
+        routerLink="/dash"
+        routerLinkActive="active"
         class="text-lg cursor-pointer align-middle"
       >
+        <i class="ml-2 fa-solid fa-chart-line"></i
+      ></a>
+      <a class="text-lg align-middle "
+        >{{ username }}<i class="ml-2 fa-regular fa-user"></i
+      ></a>
+      <a (click)="logout()" class="text-lg cursor-pointer align-middle">
         Logout<i class="ml-2 fa-solid fa-arrow-right-from-bracket"></i
       ></a>
     </div>
-  `
+  `,
 })
 export class LoginMenuComponent {
   public curUsrName: Observable<string | undefined>;
