012616b100fca5f3f513808ee2c9a1277f5327b3
Falco Weihrauch <falco@weihrauch.me>
2025-05-29 10:50:19 +0000

    update logging

--- src/services/perso/persistence/persist.scheduler.go
+++ src/services/perso/persistence/persist.scheduler.go
@@ -1,7 +1,7 @@
 package persistence
 
 import (
-	"log"
+	"os"
 
 	"github.com/Menschomat/bly.li/services/perso/logging"
 	"github.com/Menschomat/bly.li/shared/data"
@@ -16,7 +16,8 @@ var (
 func PersistUnsaved() {
 	keys, err := r.GetUnsavedKeys()
 	if err != nil {
-		log.Fatalln("There's an error with the server:", err)
+		logger.Error("There's an error with the server", "err", err)
+		os.Exit(1)
 	}
 	for _, key := range keys {
 		short := data.GetShort(key)
