15315e82eb6950dfcb3d0460f9fcaa67350dc13d
Falco Weihrauch <falco@weihrauch.me>
2025-05-29 12:35:41 +0000

    loki,prometheus,grafana working

--- src/shared/utils/logging/general.go
+++ src/shared/utils/logging/general.go
@@ -4,6 +4,10 @@ import (
 	"log/slog"
 	"os"
 	"sync"
+
+	"github.com/grafana/loki-client-go/loki"
+	slogloki "github.com/samber/slog-loki/v3"
+	slogmulti "github.com/samber/slog-multi"
 )
 
 var (
@@ -13,12 +17,20 @@ var (
 
 func getHandler() slog.Handler {
 	once.Do(func() {
-		handler = slog.NewJSONHandler(os.Stdout, nil)
+		config, _ := loki.NewDefaultConfig("http://loki:3100/loki/api/v1/push")
+		config.TenantID = "xyz"
+		client, _ := loki.New(config)
+		// Stdout handler
+		stdoutHandler := slog.NewJSONHandler(os.Stdout, nil)
+		// Loki handler
+		lokiHandler := slogloki.Option{Level: slog.LevelDebug, Client: client}.NewLokiHandler()
+
+		handler = slogmulti.Fanout(stdoutHandler, lokiHandler)
 	})
 	return handler
 }
 
 // NewLogger creates a logger with a service-name.
 func NewLogger(serviceName string) *slog.Logger {
-	return slog.New(getHandler()).With("service-name", serviceName)
+	return slog.New(getHandler()).With("service_name", serviceName)
 }
