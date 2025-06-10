bc29c9924435e2aa31254a67e6ffde70d5a3b0b9
Falco Weihrauch <falco@weihrauch.me>
2025-06-10 22:37:38 +0200

    remove echarts

--- src/services/front/src/app/components/graphs/line-chart/line-chart.component.ts
+++ src/services/front/src/app/components/graphs/line-chart/line-chart.component.ts
@@ -1,32 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
-// import echarts core
-import * as echarts from 'echarts/core';
-// import necessary echarts components
-import { BarChart, LineChart } from 'echarts/charts';
-import { GridComponent } from 'echarts/components';
-import { CanvasRenderer } from 'echarts/renderers';
 import { Observable, of } from 'rxjs';
-import { EChartsOption } from 'echarts';
-echarts.use([BarChart, LineChart, GridComponent, CanvasRenderer]);
 
 @Component({
   selector: 'app-line-chart',
-  imports: [CommonModule, NgxEchartsDirective],
+  imports: [CommonModule],
   template: `
-    <div
-      echarts
-      [options]="options$ | async"
-      [loading]="loading"
-      class="demo-chart"
-    ></div>
+
   `,
-  providers: [provideEchartsCore({ echarts })],
+  providers: [],
   styles: ``,
 })
 export class LineChartComponent {
   loading = true;
-  @Input()
-  options$: Observable<EChartsOption> = of({});
 }
