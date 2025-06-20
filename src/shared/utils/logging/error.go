614a75de81ee9d94383f08ad039c8c9b54d7a61d
Falco Weihrauch <falco@weihrauch.me>
2025-06-20 18:39:15 +0200

    Fix typo in logging messages (#17)

--- src/shared/utils/logging/error.go
+++ src/shared/utils/logging/error.go
@@ -6,11 +6,11 @@ import (
 
 func LogMongoError(err error) {
 	if err != nil {
-		slog.Error("MongoDB-Error occured", "error", err)
+		slog.Error("MongoDB error occurred", "error", err)
 	}
 }
 func LogRedisError(err error) {
 	if err != nil {
-		slog.Error("Redis-Error occured", "error", err)
+		slog.Error("Redis error occurred", "error", err)
 	}
 }
